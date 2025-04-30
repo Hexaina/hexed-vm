3d7d4f628991fa893b7a7003148d4fd8d6c5491b
Hexaina <hexas7566@gmail.com>
2025-04-30 05:24:00 +0000

    v5 instruction set is now 4-bit. added modulo, and procedures

--- hexedVm.lua
+++ hexedVm.lua
@@ -17,21 +17,34 @@ Inst = {
     sub = function() Push(Pop()-Pop()) end,
     mul = function() Push(Pop()*Pop()) end,
     div = function() Push(Pop()/Pop()) end,
+    mod = function() Push(Pop()%Pop()) end,
     ttswap = function() Push(table.remove(Stack, 2)) end,
     topdupe = function(i) local x = Pop(); for n = 0, i do Push(x) end end,
     popvar = function(name) Mem[name] = Pop() end,
     setvar = function(m) local name = m[1]; local val = m[2]; Mem[name] = val end,
     varcopy = function(name) Push(Mem[name]) end,
+    call = function(name)
+        
+        local proc = Mem[name]
+        --func is {code}
+        for x = 1, #proc do
+
+            Switch(proc[x].inst, x)
+        
+        end
+    end,
     
-
-
-
 }
 
 Program = {
-    {inst = "push", args = "cat program. please say something and i will repeat it"},
-    {inst = "print"},
-    {inst = "stread"},
+    {inst = "setvar", args = {"add2", {
+        {inst = "add"},
+    }}},
+    
+    {inst = "push", args = 2},
+    {inst = "push", args = 2},
+
+    {inst = "call", args = "add2"},
     {inst = "print"},
 }
 
