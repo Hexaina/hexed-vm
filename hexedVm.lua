6b55d30c23e32f00fed84aef3c118e1129418c81
Hexaina <hexas7566@gmail.com>
2025-04-30 20:18:05 +0000

    added loops, control flow, and boolean oporations

--- hexedVm.lua
+++ hexedVm.lua
@@ -18,11 +18,39 @@ Inst = {
     mul = function() Push(Pop()*Pop()) end,
     div = function() Push(Pop()/Pop()) end,
     mod = function() Push(Pop()%Pop()) end,
+    stequal = function() 
+        local x = Pop(); 
+        local y = Pop(); 
+        if x == y then Push(true) else Push(false) end
+    end,
+    stnotequal = function() 
+        local x = Pop(); 
+        local y = Pop(); 
+        if x ~= y then Push(true) else Push(false) end
+    end,
+    stgreater = function() 
+        local x = Pop(); 
+        local y = Pop(); 
+        if x > y then Push(true) else Push(false) end
+    end,
+    stless = function() 
+        local x = Pop(); 
+        local y = Pop(); 
+        if x < y then Push(true) else Push(false) end
+    end,
+    stand = function() Push(Pop() and Pop()) end,
+    stor = function() Push(Pop() or Pop()) end,
+    stnot = function() Push(not Pop()) end,
+    stnand = function() Push(not Pop() and Pop()) end,
+    stnor = function() Push(not Pop() or Pop()) end,
+    stxor = function() local x = Pop(); local y = Pop(); Push((x and not y) or (not x and y)) end,
+    stxnor = function() local x = Pop(); local y = Pop(); Push((x and y) or (not x and not y)) end,
     ttswap = function() Push(table.remove(Stack, 2)) end,
     topdupe = function(i) local x = Pop(); for n = 0, i do Push(x) end end,
     popvar = function(name) Mem[name] = Pop() end,
     setvar = function(m) local name = m[1]; local val = m[2]; Mem[name] = val end,
     varcopy = function(name) Push(Mem[name]) end,
+    delvar = function(name) table.remove(Mem[name]) end,
     call = function(name)
         
         local proc = Mem[name]
@@ -40,19 +68,55 @@ Inst = {
             Inst[code[x].inst](code[x].args)
         end
     end,
+    loopfor = function(m)
+        local i = m[2]
+        Mem[m[1]] = i
+        local max = m[3]
+        local inc = m[4]
+        local code = m[5]
+
+        for i = i, max, inc do
+            for x = 1, #code do
+                Inst[code[x].inst](code[x].args)
+            end
+            Mem[m[1]] = i
+        end
+    end,
+    stif = function(code)
+        local bool = Pop()
+        if bool then
+            for x = 1, #code do
+                Inst[code[x].inst](code[x].args)
+            end
+        end
+    end,
+    stifelse = function(code)
+        local bool = Pop()
+        local trcode = code[1]
+        local flcode = code[2]
+        if bool then
+            for x = 1, #trcode do
+                Inst[trcode[x].inst](trcode[x].args)
+            end
+        else
+            for x = 1, #flcode do
+                Inst[flcode[x].inst](flcode[x].args)
+            end
+        end
+    end,
 }
 
 Program = {
     
-    {inst = "push", args = 6},
-    {inst = "push", args = {
-        {inst = "push", args = 2},
-        {inst = "push", args = 2},
-        {inst = "add"},
+    {inst = "push", args = false},
+    {inst = "stifelse", args =  {{
+        {inst = "push", args = "hello"},
         {inst = "print"},
-    }},
+    }, {
+        {inst = "push", args = "bye"},
+        {inst = "print"},
+    }}},
 
-    {inst = "stexe"},
 }
 
 function Switch(v, x)
